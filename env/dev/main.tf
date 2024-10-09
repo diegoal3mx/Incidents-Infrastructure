38a26b40a8bc8a68b20d0e11d53ba12b11dd4f15
diegoal3mx <114199341+diegoal3mx@users.noreply.github.com>
2024-10-09 17:15:42 -0600

    Minor fix

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,24 +1,24 @@
 module "dev_vm" {
   source                     = "../../modules/vm"
   environment                = "dev"
-  mail_secret_key            = var.mail_secret_key
-  mail_user                  = var.mail_user
+  mail_secret_key            = var.MAIL_SECRET_KEY
+  mail_user                  = var.MAIL_USER
   admin_username             = "adminuser"
-  domain                     = var.domain
+  domain                     = var.DOMAIN
   resource_group             = "IN-RG-Diego"
   nic_name                   = "IN-NIC-Diego"
-  mail_service               = var.mail_service
-  mongo_url                  = var.mongo_url
+  mail_service               = var.MAIL_SERVICE
+  mongo_url                  = var.MONGO_URL
   security_group_name        = "IN-SG-Diego"
   ssh_key_path               = "./keys/712_Incident_Server"
   port                       = var.port
   server_name                = "IN-Server-Diego"
   location                   = "eastus2"
-  mapbox_access_token        = var.mapbox_access_token
+  mapbox_access_token        = var.MAPBOX_ACCESS_TOKEN
   subnet_name                = "IN-SUBNET-Diego"
-  mongo_initdb_root_username = var.mongo_initdb_root_username
-  mongo_initdb_root_password = var.mongo_initdb_root_password
-  mongo_db                   = var.mongo_db
+  mongo_initdb_root_username = var.MONGO_INITDB_ROOT_USERNAME
+  mongo_initdb_root_password = var.MONGO_INITDB_ROOT_PASSWORD
+  mongo_db                   = var.MONGO_DB
   public_ip_name             = "IN-IP-Diego"
   vnet_name                  = "IN_VNET_Diego"
 }
